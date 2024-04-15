b61294c97a9c19106c6203251be3c24ebff00a10
Zshah2 <148800760+Zshah2@users.noreply.github.com>
2024-04-14 23:31:23 -0400

    Create Cars.cs

---
+++ Cars.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace CarRepairTracker
+{
+    public class Car
+    {
+        public string Make { get; }
+        public string Model { get; }
+        public int Year { get; }
+        public List<Repair> Repairs { get; }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Repairs = new List<Repair>();
+        }
+
+        public override string ToString()
+        {
+            return $"{Make} {Model} ({Year})";
+        }
+    }
+}
+
+
+
+
